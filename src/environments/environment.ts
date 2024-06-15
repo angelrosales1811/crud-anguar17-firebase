c69bd6f014981246bf9b3405ca1337c5aa553416
angelrosales1811 <angeldesarrolloweb@gmail.com>
2024-06-14 23:07:40 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebase: {
+    projectId: 'crud-contacts-c985e',
+    appId: '1:205121295866:web:b3e49d4772e55cbc3b4043',
+    storageBucket: 'crud-contacts-c985e.appspot.com',
+    apiKey: 'AIzaSyBaAaJM1I6VKwChEYUVAbf6ghwK_lXqhoI',
+    authDomain: 'crud-contacts-c985e.firebaseapp.com',
+    messagingSenderId: '205121295866',
+    // projectId: 'contacts-55e11',
+    // appId: '1:161751204246:web:2633c178e847015166ddb7',
+    // storageBucket: 'contacts-55e11.appspot.com',
+    // apiKey: 'AIzaSyBViAlIFdAXXdljC0vQDl9WRravzN5Jz8Q',
+    // authDomain: 'contacts-55e11.firebaseapp.com',
+    // messagingSenderId: '161751204246',
+  },
+};
